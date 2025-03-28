2e96d3c29df82fc4df0f7d820b79b74af42b7ac3
masum184e <masum184e@gmail.com>
2025-03-29 00:48:52 +0600

    overwritten error resolved

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -28,6 +28,6 @@ const userSchema = new mongoose.Schema({
         default: Date.now,
     }
 });
-const UserModel = mongoose.model("User", userSchema);
+const UserModel = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default UserModel;
